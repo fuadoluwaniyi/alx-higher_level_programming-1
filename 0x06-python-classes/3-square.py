85e7b7c6ecf58f646a5f9d426bf5fa4069ec5fe6
Adebayo victor <111224868+1sharko@users.noreply.github.com>
2022-12-21 13:44:52 +0100

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,28 +1,27 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 
-"""Define a class Square."""
-=======
- #!/usr/bin/python3
+"""Define a MagicClass matching exactly a bytecode provided by Holberton."""
 
-"""Defines a class Square"""
->>>>>>> 33545e5b6d1dc597ff5ae0aca4912665c83c89a8
+import math
 
 
-class Square:
-    """Represent a square."""
+class MagicClass:
+    """Represent a circle."""
 
-    def __init__(self, size=0):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
+    def __init__(self, radius=0):
+        """Initialize a MagicClass.
+        Arg:
+            radius (float or int): The radius of the new MagicClass.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
 
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        """Return the area of the MagicClass."""
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        """Return The circumference of the MagicClass."""
+        return (2 * math.pi * self.__radius)
