8d5059b8d8fc3ef7d5076a510748eb3ac3ff5385
Adebayo victor <111224868+1sharko@users.noreply.github.com>
2023-01-04 21:05:44 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
-"""Module containing a function that prints two strings as a name"""
+"""
+This module is composed by a function prints a message
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints two strings as a name"""
+    """ Function that prints "My name is <first name> <last name>"
+    Args:
+        first_name: first name
+        last_name: last name
+    Returns:
+        No return
+    Raises:
+        TypeError: If first_name or last_name is not a string
+    """
+
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
+
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    print("My name is", first_name, last_name)
+
+    print("My name is {} {}".format(first_name, last_name))
